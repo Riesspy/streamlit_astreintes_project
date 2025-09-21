fdb843dccdf0e0c2a1eb251e18cda9f9f69dbb91
riadh <riadh-externe.chamki@dalkia.fr>
2025-09-21 13:42:31 +0200

    update json file

--- utils/auth.py
+++ utils/auth.py
@@ -1,6 +1,17 @@
 import json
+import os
 
 def load_users(filepath="data/users.json"):
+    if not os.path.exists(filepath):
+        # Valeurs par défaut
+        return {
+            "JULIE123": "Julie",
+            "LYNDA456": "Lynda",
+            "RIADH789": "Riadh",
+            "ESTELLE000": "Estelle",
+            "FLORIAN321": "Florian",
+            "MATHIAS654": "Mathias"
+        }
     with open(filepath, "r") as f:
         return json.load(f)
 
